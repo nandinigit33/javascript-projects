fe72a76c9eee11d4ce2d76595cbc2bbc177066e1
nandinigit33 <nandinids33@gmail.com>
2024-07-16 00:19:41 -0400

    committing studio Functions assignment

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -8,7 +8,15 @@
 // 4. Below the function, define and initialize a variable to hold a string.
 // 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
+/*function reverseCharacters(str) {
+return str.split("").reverse().join("");
+elbissop
+return str.split("").reverse().join();
+e,l,b,i,s,s,o,p
 
+let myVariableName = "possible";
+let reversedString = console.log(reverseCharacters(myVariableName));
+*/
 // Part Two: Reverse Digits
 
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
@@ -16,6 +24,16 @@
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
 // 4. Return the reversed number.
 // 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
+function reverseCharacters(someString) {
+  if (typeof someString === "string") {
+    return someString.split("").reverse().join("");
+  } else if (typeof someString === "number") {
+    return Number(String(someString).split("").reverse().join(""));
+  }
+}
+
+console.log(reverseCharacters(1234));
+console.log(reverseCharacters("name"));
 
 // Part Three: Complete Reversal
 
@@ -26,9 +44,21 @@
 // 5. Return the final, reversed array.
 // 6. Be sure to print the results from each test case in order to verify your code.
 
-let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
+let arrayTest1 = ["apple", "potato", "Capitalized Words"];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
-let arrayTest3 = ['hello', 'world', 123, 'orange'];
+let arrayTest3 = ["hello", "world", 123, "orange"];
+
+function completeReversal(someArray) {
+  let newArr = [];
+  for (let i = 0; i < someArray.length; i++) {
+    newArr.push(reverseCharacters(someArray[i]));
+  }
+  return newArr.reverse();
+}
+
+console.log(completeReversal(arrayTest1));
+console.log(completeReversal(arrayTest2));
+console.log(completeReversal(arrayTest3));
 
 // Bonus Missions
 
@@ -36,12 +66,23 @@ let arrayTest3 = ['hello', 'world', 123, 'orange'];
 // 2. Retrieve only the last character from strings with lengths of 3 or less.
 // 3. Retrieve only the first 3 characters from strings with lengths larger than 3.
 // 4. Use a template literal to return the phrase We put the '___' in '___'. Fill the first blank with the modified string, and fill the second blank with the original string.
-
+function modifyString(str) {
+  let subString;
+  if (str.length <= 3) {
+    subString = str[str.length - 1];
+  } else {
+    subString = str.slice(0, 3);
+  }
+  return `We put the ${subString} in ${funString}.`;
+}
 // Test Function
 
 // 1. Outside of the function, define the variable str and initialize it with a string (e.g. 'Functions rock!').
 // 2. Call your function and print the returned phrase.
-
+let funString = "Function Rocks!";
+let shortFunString = "We";
+console.log(modifyString(funString));
+console.log(modifyString(shortFunString));
 // Area of rectangle equal to length x width
 
 // 1. Define a function with the required parameters to calculate the area of a rectangle.
@@ -49,3 +90,9 @@ let arrayTest3 = ['hello', 'world', 123, 'orange'];
 // 3. Call your area function by passing in two arguments - the length and width.
 // 4. If only one argument is passed to the function, then the shape is a square. Modify your code to deal with this case.
 // 5. Use a template literal to print, “The area is ____ cm^2.”
+
+function getRectangleArea(length, width = length) {
+  return length * width;
+}
+console.log(`“The area is ${getRectangleArea(6, 4)} cm^2.”`);
+console.log(`“The area is ${getRectangleArea(6)} cm^2.”`);
